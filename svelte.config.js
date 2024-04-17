b5fd7c67d2d1c6096b2dddd220d8084f68247fc5
yugi-1 <10856389@uvu.edu>
2024-04-16 20:41:29 -0600

    if this doesnt work...

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,9 +15,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: 'index.html' // may differ from host to host
-		})
+		adapter: adapter()
 	}
 };
 export default config;
