5dabdae9a14b4e1083c7ee97ef53dfee98bdff12
yugi-1 <10856389@uvu.edu>
2024-04-16 20:45:50 -0600

    if this doesnt work...

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,12 @@ const config = {
 		inspector: true,
 	},
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'app.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 export default config;
