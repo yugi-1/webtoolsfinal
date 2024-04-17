a9888b880385fb739a77bcbf0005518f2c275eef
yugi-1 <10856389@uvu.edu>
2024-04-16 21:31:54 -0600

    omg

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,17 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'app.html' // may differ from host to host
-		})
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : 'https://yugi-1.github.io/webtoolsfinal/'
+		}
 	}
 };
 export default config;
