f3dcd6c2eed2a5b015b92139ed1951708d468b42
yugi-1 <10856389@uvu.edu>
2024-04-16 21:16:45 -0600

    omg

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,14 +8,13 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess()],
 	
-	vitePlugin: {
-		inspector: true,
-	},
 	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html'
+			fallback: 'index.html' // may differ from host to host
 		})
-		
 	}
 };
 export default config;
